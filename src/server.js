b08615c23efd65763e08990b4be45a825a3364aa
Mateusvr <mateusvr2005@outlook.com>
2023-07-03 21:05:05 -0300

    fim da aula 03-07

--- src/server.js
+++ src/server.js
@@ -4,12 +4,22 @@ const port = 3000;
 const users = require('./data/users.json')
 
 app.get('/', (req, res) => {
-    res.send('Servidor Rodando!');
+    res.send('Que lindo, cara!');
 });
 
 app.get('/users', (req, res) => {
     res.send(users).json()
 });
+
+app.get('/user/:id', (req, res) =>{
+    let idParam = parseInt(req.params.id); 
+    
+    console.log('users', users)
+
+    let userFiltered = users.filter(user => user.id === idParam)
+
+    res.status(200).send(userFiltered)
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`);
